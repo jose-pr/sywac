514a78c63e9819bb382a0872154802b8f9243279
Jose A <jose-pr@coqui.dev>
2020-06-05 18:22:16 +0300

    version type

---
+++ src/types/version.ts
@@ -0,0 +1,58 @@
+import TypeImplicitCommand, { TypeImplicitCommandOptions } from './implicit'
+import { Context, DeferVersion } from '../context'
+
+export interface TypeVersionOptions extends DeferVersion, TypeImplicitCommandOptions { }
+
+class TypeVersion extends TypeImplicitCommand {
+  static get(opts: TypeVersionOptions) {
+    return new TypeVersion(opts)
+  }
+
+  private _versionOpts?: TypeImplicitCommandOptions
+
+  constructor(opts?: TypeVersionOptions) {
+    super(Object.assign({ desc: 'Show version number' }, opts))
+  }
+
+  configure(opts?: TypeVersionOptions, override?: boolean) {
+    opts = opts || {} as TypeVersionOptions
+    if (typeof override === 'undefined') override = true
+    super.configure(opts, override)
+
+    if (override || !this._versionOpts) this._versionOpts = this._assignVersionOpts(this._versionOpts || {} as TypeVersionOptions, opts)
+
+    return this
+  }
+
+  _assignVersionOpts(target: TypeVersionOptions, source: TypeVersionOptions) {
+    (['version'] as (keyof TypeVersionOptions)[]).forEach(opt => {
+      if (opt in source) (target[opt] as TypeVersionOptions[typeof opt]) = source[opt] as any   
+    })
+    return target
+  }
+
+  get versionOpts(): TypeVersionOptions {
+    return this._versionOpts || {} as TypeVersionOptions
+  }
+
+  validateConfig(utils: any) {
+    if (!this._flags && !this._aliases.length) this._aliases.push('version')
+    super.validateConfig(utils)
+  }
+
+  validateParsed(context: Context) {
+    if (this.getValue(context)) this.requestVersion(context) // must call this before postParse in case of commands
+    return this.resolve()
+  }
+
+  implicitCommandFound(context: Context, source: string, position: number, raw: string) {
+    super.implicitCommandFound(context, source, position, raw)
+    this.requestVersion(context) // must call this before postParse in case of commands
+  }
+
+  requestVersion(context: Context) {
+    context.deferVersion(this.versionOpts)
+  }
+}
+
+module.exports = TypeVersion
